0197a528b0b068bc4dff5a8c220f781ba0a15a40
Decard <mail@dayreon.ru>
2023-12-10 18:14:35 +0800

    v0.1.0

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Lemerald";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+  outputs = inputs@{ self, nixpkgs, home-manager, hyprland, ... }: {
+    nixosConfigurations = {
+      lemerald = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/configuration.nix
+          hyprland.nixosModules.default
+          { programs.hyprland.enable = true; }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.decard = import ./home.nix;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+          }
+        ];
+      };
+    };
+  };
+}
