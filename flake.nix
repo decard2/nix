ba86307277fbdeef7b2f5c7ad7230f46a2ce6a31
Decard <mail@dayreon.ru>
2023-12-11 02:46:41 +0800

    firefox

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "github:hyprwm/Hyprland";    
   };
   outputs = inputs@{ self, nixpkgs, home-manager, hyprland, ... }: {
     nixosConfigurations = {
