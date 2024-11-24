8e47795532d0328be458ed46cb64ae16d2fd6f7c
decard <mail@decard.space>
2024-11-24 08:55:58 +0800

    45

--- nixos/disko.nix
+++ nixos/disko.nix
@@ -2,28 +2,28 @@
   disko.devices = {
     disk = {
       vda = {
-        device = "/dev/vda";
         type = "disk";
+        device = "/dev/vda";
         content = {
           type = "gpt";
           partitions = {
             boot = {
+              name = "boot";            # Добавим name
               size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                label = "boot";  # Добавили метку
               };
             };
             root = {
+              name = "root";            # Добавим name
               size = "100%";
               content = {
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
-                label = "nixos";  # Добавили метку
               };
             };
           };
