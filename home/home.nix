ad6e946aaec6716f0fe5367b88fed7e335341fd0
decard <mail@decard.space>
2024-11-25 06:32:40 +0800

    77

--- home/home.nix
+++ home/home.nix
@@ -80,15 +80,19 @@ in
   home.file.".config/tofi/config".text = ''
     # Основные настройки
     width = 100%
-    height = 40
+    height = 150
     border-width = 0
     outline-width = 0
-    padding-left = 35%
-    padding-top = 20
-    result-spacing = 15
-    num-results = 5
+    padding-left = 15
+    padding-top = 8
+    result-spacing = 25
+    num-results = 7
     font = Noto Sans
     font-size = 14
+
+    # Позиционирование
+    anchor = top
+    horizontal = true
 
     # Цвета (максимально простые)
     background-color = #000000A0
