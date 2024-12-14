d84a543edcaaa50821e5bbba6102adf9fc0aaac9
decard <mail@decard.space>
2024-12-14 20:39:28 +0800

    joara keys secured

---
+++ home/programs/bitwarden.nix
@@ -0,0 +1,21 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    rbw
+    pinentry
+  ];
+
+  home.file.".config/rbw/config.json".text = ''
+    {
+      "email": "mail@decard.space",
+      "sso_id":null,
+      "base_url":"https://vault.decard.space",
+      "identity_url":null,
+      "ui_url":null,
+      "notifications_url":null,
+      "lock_timeout":2592000,
+      "sync_interval":3600,
+      "pinentry":"pinentry",
+      "client_cert_path":null
+    }
+  '';
+}
