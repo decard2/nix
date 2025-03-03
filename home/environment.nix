8aab07ca56db73e620957ea0fc9fba01409d562b
decard <mail@decard.space>
2025-03-04 03:17:58 +0800

    gtk3 fix

--- home/environment.nix
+++ home/environment.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 let
   vars = {
     VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/intel_icd.x86_64.json";
@@ -8,6 +9,7 @@ let
     XKB_CONFIG_ROOT = "/run/current-system/sw/share/X11/xkb";
     PATH = "$HOME/.local/bin:$PATH";
     DEEPSEEK_API_KEY = "sk-35039be1f9084afe802f95f4e8e331a7";
+    GSETTINGS_SCHEMA_DIR = "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}/glib-2.0/schemas";
   };
 in
 {
