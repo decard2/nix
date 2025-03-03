e0947b3c6d5e142aaa609e060a5af92d9bf5c9ab
decard <mail@decard.space>
2025-03-04 02:46:59 +0800

    file associations

---
+++ home/de/fileAssociations.nix
@@ -0,0 +1,9 @@
+{
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "inode/directory" = "thunar.desktop";
+      "application/x-gnome-saved-search" = "thunar.desktop";
+    };
+  };
+}
