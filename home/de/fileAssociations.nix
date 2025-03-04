a46abf943c71c0c800c78ef5bb8fcd91942bbf4b
decard <mail@decard.space>
2025-03-04 19:22:37 +0800

    fileAssociations, abbrs for flox and cargo

--- home/de/fileAssociations.nix
+++ home/de/fileAssociations.nix
@@ -4,6 +4,20 @@
     defaultApplications = {
       "inode/directory" = "thunar.desktop";
       "application/x-gnome-saved-search" = "thunar.desktop";
+
+      "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
+      "x-scheme-handler/tonsite" = "org.telegram.desktop.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/chrome" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html" = "firefox.desktop";
+      "application/x-extension-shtml" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+      "application/x-extension-xhtml" = "firefox.desktop";
+      "application/x-extension-xht" = "firefox.desktop";
+      "application/pdf" = "firefox.desktop";
     };
   };
 }
