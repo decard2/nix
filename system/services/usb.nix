7629f89cc97fb8dfc73852ab7abb51dce11c7fce
decard <mail@decard.space>
2025-03-23 18:03:03 +0800

    Enable gvfs and add usbutils to system packages

--- system/services/usb.nix
+++ system/services/usb.nix
@@ -2,13 +2,8 @@
 {
   services = {
     gvfs.enable = true;
-    udisks2.enable = true;
   };
   environment.systemPackages = with pkgs; [
-    # udiskie
     usbutils
-    # android-udev-rules
-    # gvfs
-    # jmtpfs
   ];
 }
