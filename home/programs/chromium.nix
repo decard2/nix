b66f32ddce17710aa3e50c283a870db347139641
decard <mail@decard.space>
2025-05-11 10:41:45 +0800

    Move Chromium args to package override with minimal Wayland config

--- home/programs/chromium.nix
+++ home/programs/chromium.nix
@@ -2,20 +2,12 @@
 {
   programs.chromium = {
     enable = true;
-    commandLineArgs = [
-      "--enable-features=VaapiVideoDecodeLinuxGL"
-      "--enable-zero-copy"
-      "--force-dark-mode"
-      "--ozone-platform=wayland"
-      "--enable-features=UseOzonePlatform"
-      "--enable-features=WebRTCPipeWireCapturer"
-      "--enable-features=Vulkan"
-      "--use-vulkan"
-      "--enable-gpu-rasterization"
-      "--ignore-gpu-blacklist"
-    ];
     package = pkgs.chromium.override {
       enableWideVine = true; # Для поддержки DRM контента
+      commandLineArgs = [
+        "--enable-features=UseOzonePlatform"
+        "--ozone-platform=wayland"
+      ];
     };
   };
 }
