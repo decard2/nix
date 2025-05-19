e1b8707fcd708f55c049814e0db6d40842c1eeb1
decard <mail@decard.space>
2025-05-20 02:29:54 +0800

    Update Transmission configuration and fix directory structure
    Move directories creation to user tmpfiles and reorganize paths for
    incomplete downloads to be directly under torrents folder

--- system/services/transmission.nix
+++ system/services/transmission.nix
@@ -7,8 +7,10 @@
     package = pkgs.transmission_4;
     settings = {
       download-dir = "/home/decard/torrents/downloads";
-      watch-dir = "/home/decard/torrents/torrents";
       watch-dir-enabled = true;
+      watch-dir = "/home/decard/torrents/torrents";
+      incomplete-dir-enabled = true;
+      incomplete-dir = "/home/decard/torrents/.incomplete";
 
       rpc-enabled = true;
       rpc-port = 9091;
@@ -20,23 +22,20 @@
       ratio-limit = 2.0;
       ratio-limit-enabled = false;
 
-      incomplete-dir = "/home/decard/torrents/downloads/.incomplete";
-      incomplete-dir-enabled = true;
       peer-port = 51413;
       peer-port-random-on-start = false;
     };
   };
 
+  systemd.user.tmpfiles.users.decard.rules = [
+    "d /home/decard/torrents/downloads 0755 decard users -"
+    "d /home/decard/torrents/torrents 0755 decard users -"
+    "d /home/decard/torrents/.incomplete 0755 decard users -"
+  ];
+
   # Добавляем нужные пакеты в систему
   environment.systemPackages = with pkgs; [
     transmission_4
     tremc
-  ];
-
-  # Создаём нужные директории при установке
-  systemd.tmpfiles.rules = [
-    "d /home/decard/torrents/downloads 0755 decard users -"
-    "d /home/decard/torrents/torrents 0755 decard users -"
-    "d /home/decard/torrents/downloads/.incomplete 0755 decard users -"
   ];
 }
