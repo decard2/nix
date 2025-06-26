22065aeb744c1683b635f6fd2bed53fe39e59a16
decard <mail@decard.space>
2025-06-26 15:52:28 +0800

    Add Wayland support for Yandex Browser

--- home/programs/yandex-browser.nix
+++ home/programs/yandex-browser.nix
@@ -1,6 +1,17 @@
 { pkgs, inputs, ... }:
+let
+  yandex-browser-wayland = pkgs.writeShellScriptBin "yandex-browser-wayland" ''
+    exec ${
+      inputs.yandex-browser.packages.${pkgs.system}.yandex-browser-stable
+    }/bin/yandex-browser-stable \
+      --ozone-platform-hint=auto
+      "$@"
+  '';
+in
 {
-  home.packages = with inputs.yandex-browser.packages.${pkgs.system}; [
-    yandex-browser-stable
+  home.packages = [
+    inputs.yandex-browser.packages.${pkgs.system}.yandex-browser-stable
+    yandex-browser-wayland
   ];
+
 }
