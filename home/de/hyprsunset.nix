4067b5b42f8f532debcbeb78694dec97a7f0d10c
decard <mail@decard.space>
2025-07-21 19:43:07 +0800

    Fix hyprsunset 0.3.0 build
    - Fixed hash for v0.3.0 source
    - Added hyprlang dependency required for 0.3.0
    - Package now builds successfully and shows correct version

--- home/de/hyprsunset.nix
+++ home/de/hyprsunset.nix
@@ -8,8 +8,10 @@ let
       owner = "hyprwm";
       repo = "hyprsunset";
       tag = "v0.3.0";
-      hash = "sha256-DQCLsz1+F4cAmm/fz81Nyx3ZxMTEv/Phz0T0xfpmlqo=";
+      hash = "sha256-Cp5kHNfWFFyJpA5eRxZh/7fJcHU1uM2tBehQyeyKEUA=";
     };
+
+    buildInputs = oldAttrs.buildInputs ++ [ pkgs.hyprlang ];
 
     meta = oldAttrs.meta // {
       description = "Application to enable a blue-light filter on Hyprland (version 0.3.0)";
