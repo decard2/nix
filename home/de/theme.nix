95a173d7cca459cd1c1656f68b870511478d70eb
decard <mail@decard.space>
2026-04-06 18:04:31 +0800

    Update theme.nix

--- home/de/theme.nix
+++ home/de/theme.nix
@@ -34,6 +34,10 @@
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = true;
     };
+    gtk4.theme = {
+      name = "Adwaita";
+      package = pkgs.gnome-themes-extra;
+    };
     gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = true;
     };
